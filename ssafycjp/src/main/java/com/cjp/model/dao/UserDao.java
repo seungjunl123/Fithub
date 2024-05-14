a9db667f1df1a551d83c667c6262f9e080c7a307
kts <tskim4065@gmail.com>
2024-05-14 15:17:38 +0900

    BE_SpringProjectCreated

---
+++ ssafycjp/src/main/java/com/cjp/model/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.cjp.model.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.cjp.model.dto.User;
+
+public interface UserDao {
+	public List<User> selectAll();
+	public void insertUser(User user);
+	public User selectOne(Map<String, String> info);
+}
