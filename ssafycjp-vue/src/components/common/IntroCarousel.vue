42416270b2fa9bb06aa708e77b10a5cfa527f0a8
kts <tskim4065@gmail.com>
2024-05-23 17:09:34 +0900

    update

--- ssafycjp-vue/src/components/common/IntroCarousel.vue
+++ ssafycjp-vue/src/components/common/IntroCarousel.vue
@@ -1,51 +1,37 @@
-gi<template>
-    <v-carousel
-      height="400"
-      show-arrows="hover"
-      cycle
-      hide-delimiter-background
-    >
-      <v-carousel-item
-        v-for="(slide, i) in slides"
-        :key="i"
-      >
-        <v-sheet
-          :color="colors[i]"
-          height="100%"
-        >
-          <div class="d-flex fill-height justify-center align-center">
-            <div class="text-h2">
-              {{ slide }}
-            </div>
-          </div>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
-  </template>
+<template>
+  <v-carousel :show-arrows="false" height="600">
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      cover
+    ></v-carousel-item>
+  </v-carousel>
+</template>
 
 <script>
-  export default {
-    data () {
-      return {
-        colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ],
-        slides: [
-          '야호오오오',
-          '기분좋다아ㅏ',
-          '드가자ㅏ',
-          '운동하자ㅏ',
-          '기모띠!!',
-        ],
-      }
-    },
-  }
+import Main1 from '@/assets/Main1.jpg';
+import Main2 from '@/assets/Main2.jpg';
+import Main3 from '@/assets/Main3.jpg';
+
+export default {
+  data () {
+    return {
+      items: [
+        {
+          src: Main1,
+        },
+        {
+          src: Main2,
+        },
+        {
+          src: Main3,
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
